989e361715ae28f187f4b194b751b83c94abf39b
Oden <oden.srtk@gmail.com>
2022-11-12 21:51:39 +0900

    for pre release

--- src/response.rs
+++ src/response.rs
@@ -1,14 +1,14 @@
 use std::{fs::File, io::Read};
 
 use hyper::Body;
-use semilattice_client_lib::SemilatticeClient;
+use wild_doc_client_lib::WildDocClient;
 
 pub(crate) fn make(filename:&str)->Body{
     let mut contents = Vec::new();
     if let Ok(mut f) = File::open(filename){
         let mut sml=String::new();
         if let Ok(_)=f.read_to_string(&mut sml){
-            let mut sc=SemilatticeClient::new("document");
+            let mut sc=WildDocClient::new("document");
             contents=sc.exec(&sml);
         }
     }
