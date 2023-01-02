e5caba3b349bce3bfda30baf93b616e04e856b47
Oden <oden.srtk@gmail.com>
2023-01-02 14:02:06 +0900

    base64encode to upload file data

--- src/request.rs
+++ src/request.rs
@@ -51,7 +51,7 @@ struct UploadFile {
     file_name: String,
     content_type: String,
     len: usize,
-    data: Vec<u8>,
+    data: String,   //(base64)
 }
 struct UploadFileWrapper {
     key: String,
@@ -141,7 +141,7 @@ pub(super) async fn request(
                                                     file_name: file_name.to_owned(),
                                                     content_type: content_type.to_string(),
                                                     len: chunk.len(),
-                                                    data: chunk.to_vec(),
+                                                    data: base64::encode(chunk),
                                                 },
                                             });
                                         } else {
